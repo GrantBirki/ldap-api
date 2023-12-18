3b7f64feeb038c65622c020a6fd9e9cd1c923b9f
GrantBirki <grantbirki@github.com>
2023-12-18 13:04:33 -0700

    more formatting

--- lib/ldap2rest/api.rb
+++ lib/ldap2rest/api.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
+
+require "grape"
+require "garner"
+
 module Ldap2Rest
   module API
     class LDAP < Grape::API
       use Garner::Middleware::Cache::Bust
       helpers Garner::Mixins::Grape::Cache
 
-      version "v1", :using => :path
+      version "v1", using: :path
       format :json
       default_format :json
 
@@ -13,7 +17,7 @@ module Ldap2Rest
 
       helpers do
         def build_filter(model, value)
-          value = "*" unless value
+          value ||= "*"
           Settings.ldap.send(model).filter.gsub("%s", value) if Settings.ldap.send(model).filter
         end
       end
@@ -23,31 +27,33 @@ module Ldap2Rest
         get do
           @users = cache do
             filter ||= build_filter(:user, params[:filter])
-            Ldap2Rest::User.find(:all, { :filter => filter, :limit => Settings.ldap.limit_results }).collect { |x| x.to_os }
+            Ldap2Rest::User.find(:all, { filter:, limit: Settings.ldap.limit_results }).collect do |x|
+              x.to_os
+            end
           end
-          present @users, :with => Ldap2Rest::API::User
+          present @users, with: Ldap2Rest::API::User
         end
 
         desc "Returns a single user matching specified username"
         get ":username" do
           params do
-            requires :username, :type => String, :desc => "username to be fetched"
+            requires :username, type: String, desc: "username to be fetched"
           end
           @user = cache do
             @user = Ldap2Rest::User.find(:first, params[:username])
             @user.to_os if @user
           end
-          present @user, :with => Ldap2Rest::API::User
+          present @user, with: Ldap2Rest::API::User
         end
 
         get ":username/groups" do
           params do
-            requires :username, :type => String, :desc => "username to be fetched"
+            requires :username, type: String, desc: "username to be fetched"
           end
           @groups = cache do
             Ldap2Rest::User.find(:first, params[:username]).groups.collect { |x| x.to_os }
           end
-          present @groups, :with => Ldap2Rest::API::Group
+          present @groups, with: Ldap2Rest::API::Group
         end
       end
 
@@ -56,23 +62,24 @@ module Ldap2Rest
         get do
           @groups = cache do
             filter ||= build_filter(:group, params[:filter])
-            Ldap2Rest::Group.find(:all, { :filter => filter, :limit => Settings.ldap.limit_results }).collect { |x| x.to_os }
+            Ldap2Rest::Group.find(:all, { filter:, limit: Settings.ldap.limit_results }).collect do |x|
+              x.to_os
+            end
           end
-          present @groups, :with => Ldap2Rest::API::Group
+          present @groups, with: Ldap2Rest::API::Group
         end
 
         desc "Returns a list of groups from LDAP matching specified filter"
         get ":name/members" do
           params do
-            requires :filter, :type => String, :desc => "filter by group name. Wildcard(*) should be used"
+            requires :filter, type: String, desc: "filter by group name. Wildcard(*) should be used"
           end
           @users = cache do
             Ldap2Rest::Group.find(:first, params[:name]).members.collect { |x| x.to_os }
           end
-          present @users , :with => Ldap2Rest::API::User
+          present @users, with: Ldap2Rest::API::User
         end
       end
-
     end
   end
 end
--- lib/ldap2rest/settings.rb
+++ lib/ldap2rest/settings.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
+
 module Ldap2Rest
-
   module LdapSettings
-
     def self.setup(path = "./config/config.yml")
       RailsConfig.load_and_set_settings path
       settings = Settings.ldap.connection.to_hash
       validate_config
-      settings[:logger] = Logger.new(STDERR)
+      settings[:logger] = Logger.new($stderr)
       settings[:logger].level = Logger::DEBUG
-      settings[:allow_anonymous]= false unless settings.has_key? :allow_anonymous
+      settings[:allow_anonymous] = false unless settings.key? :allow_anonymous
       ActiveLdap::Base.setup_connection settings
       require "./lib/ldap2rest/active_ldap"
       require "./lib/ldap2rest/api/entities"
@@ -20,16 +19,15 @@ module Ldap2Rest
       h.assert_valid_keys(:ldap)
       h[:ldap].assert_valid_keys(:connection, :cache, :limit_results, :user, :group)
       c = h[:ldap][:connection]
-      [:host, :base].each do |k|
-        raise(ArgumentError, "Need ldap.connection.key: #{k}") unless h[:ldap][:connection].has_key?(k)
+      %i[host base].each do |k|
+        raise(ArgumentError, "Need ldap.connection.key: #{k}") unless h[:ldap][:connection].key?(k)
       end
-      [:dn_attribute, :classes, :attributes].each do |k|
-        raise(ArgumentError, "Need key: ldap.user.#{k}") unless h[:ldap][:user].has_key?(k)
+      %i[dn_attribute classes attributes].each do |k|
+        raise(ArgumentError, "Need key: ldap.user.#{k}") unless h[:ldap][:user].key?(k)
       end
-      [:dn_attribute, :classes, :member_attribute, :user_membership_attribute, :attributes].each do |k|
-        raise(ArgumentError, "Need key: ldap.group.#{k}") unless h[:ldap][:group].has_key?(k)
+      %i[dn_attribute classes member_attribute user_membership_attribute attributes].each do |k|
+        raise(ArgumentError, "Need key: ldap.group.#{k}") unless h[:ldap][:group].key?(k)
       end
     end
-
   end
 end
