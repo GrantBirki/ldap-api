2dfbe95aa76a03932f43be49b89033358d47b374
GrantBirki <grantbirki@github.com>
2024-01-09 15:59:12 -0700

    add extra users test

--- spec/acceptance/tests/users_spec.rb
+++ spec/acceptance/tests/users_spec.rb
@@ -5,7 +5,7 @@ require "json"
 require_relative "../../spec_helper"
 
 describe "API call to users endpoint" do
-  it "returns valid data" do
+  it "returns valid data for all users" do
     uri = URI("http://localhost:8080/v1/users")
     response = Net::HTTP.get(uri)
     users = JSON.parse(response)
@@ -41,5 +41,31 @@ describe "API call to users endpoint" do
     ]
 
     expect(users).to eq(expected_users)
+  end
+
+  it "returns valid data for a single user" do
+    uri = URI("http://localhost:8080/v1/users/russianblue")
+    response = Net::HTTP.get(uri)
+    user = JSON.parse(response)
+
+    expected_user = {
+      "uid" => "russianblue",
+      "uidnumber" => 20_257
+    }
+
+    expect(user).to eq(expected_user)
+  end
+
+  it "returns valid data for a single user's group memberships" do
+    uri = URI("http://localhost:8080/v1/users/russianblue/groups")
+    response = Net::HTTP.get(uri)
+    user = JSON.parse(response)
+
+    expected_response = [
+      { "name" => "colonel-meow" },
+      { "name" => "garfield" }
+    ]
+
+    expect(user).to eq(expected_response)
   end
 end
